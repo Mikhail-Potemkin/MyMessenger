71d8180e715bbe5ed90677ddcabfdf6cdc5b5ac4
Mikhail Potemkin <potemkinmr@gmail.com>
2021-12-02 23:27:37 +0300

    Добавлена сериализация и десериализация строки, а также пример выведенной строки

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace MyMessenger
 {
@@ -6,9 +7,13 @@ namespace MyMessenger
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine("Start Project!");
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("Mikhail", "Hello", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //{"UserName":"Mikhail","MessageText":"Hello","TimeStamp":"2021-12-02T20:19:47.3175332Z"}
+            //Mikhail < 02.12.2021 20:19:47 >: Hello
         }
     }
 }
