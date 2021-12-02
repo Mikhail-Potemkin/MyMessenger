242a14337e7e788eef555575e30404f9a013e750
Mikhail Potemkin <potemkinmr@gmail.com>
2021-12-02 20:47:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyMessenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
