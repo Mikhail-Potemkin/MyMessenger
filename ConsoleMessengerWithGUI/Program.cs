d8083855632844aad6ef72eda4044ef0713f0ad1
Mikhail Potemkin <potemkinmr@gmail.com>
2021-12-09 23:03:28 +0300

    Добавлен клиент на основе TerminalGui

---
+++ ConsoleMessengerWithGUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using MyMessenger;
+using System;
+using System.Collections.Generic;
+using Terminal.Gui;
+
+namespace ConsoleMessengerWithGUI
+{
+    class Program
+    {
+        private static List<Message> messages = new List<Message>();
+        private static MessengerClientAPI API = new MessengerClientAPI();
+
+        private static MenuBar menu;
+        private static Window winMain;
+        private static Window winMessages;
+        private static Label labelUser;
+        private static TextField fieldUsername;
+        private static Label labelMessage;
+        private static TextField fieldMessage;
+        private static Button btnSend;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
