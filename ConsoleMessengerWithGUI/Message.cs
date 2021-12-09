ee92e76d91c69c3f7935820615a39262b79db099
Mikhail Potemkin <potemkinmr@gmail.com>
2021-12-09 23:25:51 +0300

    fix ConsoleMessengerWithGUI

--- ConsoleMessengerWithGUI/Message.cs
+++ ConsoleMessengerWithGUI/Message.cs
@@ -1,34 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace MyMessenger
+namespace ConsoleMessenger
 {
-    [Serializable]
-    public class Message
-    {
-        public string UserName { get; set; }
-        public string MessageText { get; set; }
-        public DateTime TimeStamp { get; set; }
-        public Message(string userName, string messageText, DateTime timeStamp)
-        {
-            UserName = userName;
-            MessageText = messageText;
-            TimeStamp = timeStamp;
-        }        
-        public Message()
-        {
-            UserName = "System";
-            MessageText = "Server is running...";
-            TimeStamp = DateTime.Now;
-        }
+  [Serializable]
+  public class Message
+  {
+    public string UserName { get; set; }
+    public string MessageText { get; set; }
+    public DateTime TimeStamp { get; set; }
 
-        public override string ToString()
-        {
-            string output = string.Format("{0} <{2}>: {1}", UserName, MessageText, TimeStamp);
-            return output;
-        }
+    public Message()
+    {
+      UserName = "System";
+      MessageText = "Server is running";
+      TimeStamp = DateTime.Now;
     }
+
+    public Message(string userName, string messageText, DateTime timeStamp)
+    {
+      UserName = userName;
+      MessageText = messageText;
+      TimeStamp = timeStamp;
+    }
+
+    public override string ToString()
+    {
+      string output = String.Format("{0} <{2}>: {1}", UserName, MessageText, TimeStamp);
+      return output;
+    }
+  }
 }
